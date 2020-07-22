f8074fa4f2abc93a822625eee0596f80bd563c88
Muhammed Shah <muhammedshah@live.in>
2020-07-22 17:47:02 +0530

    replaced default tempalate with angular-folder-strucutre-repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from '@app/core.module';
+import { SharedModule } from '@shared/shared.module';
+
 import { AppComponent } from './app.component';
-import { EditorsPickComponent } from './homepage/editors-pick/editors-pick.component';
-import { NewsFeedComponent } from './homepage/news-feed/news-feed.component';
-import { NavbarComponent } from './common/navbar/navbar.component';
-import { FooterComponent } from './common/footer/footer.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
+import { NavComponent } from './layout/nav/nav.component';
+import { FooterComponent } from './layout/footer/footer.component';
+
+import { AuthModule } from '@modules/auth/auth.module';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EditorsPickComponent,
-    NewsFeedComponent,
-    NavbarComponent,
-    FooterComponent
+    ContentLayoutComponent,
+    NavComponent,
+    FooterComponent,
+    AuthLayoutComponent
   ],
   imports: [
+    // angular
     BrowserModule,
-    AppRoutingModule
+
+    // 3rd party
+    AuthModule,
+
+    // core & shared
+    CoreModule,
+    SharedModule,
+
+    // app
+    AppRoutingModule,
+
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
